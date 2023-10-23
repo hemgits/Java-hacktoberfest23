eb0fb1fa92aba8ff0c40ba49b1e4e940ef0c8dd2
HEMANT SONI <125587034+hemgits@users.noreply.github.com>
2023-10-23 20:26:17 +0530

    Addition.java
    with the help of this program we are able to sum any two number.
    #hactoberfest contribution.

---
+++ Addition.java
@@ -0,0 +1,19 @@
+import java.util.scanner;
+class sum
+{
+  public static void main(String[]args)
+{
+  
+Scanner SC = new scanner(System.in);
+ 
+ int a,b;
+
+System.out.println("ENTER FIRST NO. ");
+a=SC.nextInt();
+System.out.println("ENTER SECOND NO. ");
+b=SC.nextInt();
+
+System.out.println("SUM IS "+(a+b));
+
+}
+}
